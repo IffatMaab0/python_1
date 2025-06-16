586442a4533a813e6dda420eee18903372056fb6
IffatMaab0 <iffatmaab11@gmail.com>
2025-06-17 02:11:42 +0500

    project

--- drinkWaterreminder.py
+++ drinkWaterreminder.py
@@ -1,13 +1,27 @@
+# import time
+# import plyer
+
+# def remind_to_drink():
+#     plyer.notification.notify(
+#         title = "Drink Water Reminder",
+#         message = "Time to drink a glass of water 💧",
+#         timeout = 10
+#     )
+
+# while True:
+#     remind_to_drink()
+#     time.sleep(3600) 
+
+
+    #####using os module#### 
+
+
+import os
 import time
-import plyer
 
 def remind_to_drink():
-    plyer.notification.notify(
-        title = "Drink Water Reminder",
-        message = "Time to drink a glass of water 💧",
-        timeout = 10
-    )
+    os.system('msg * " Drink Water Reminder: Time to hydrate!"')
 
 while True:
     remind_to_drink()
-    time.sleep(3600) 
+    time.sleep(1800) 
