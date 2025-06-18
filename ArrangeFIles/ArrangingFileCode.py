71836cbdc3dd6e30d994299e853f92282db71f9b
IffatMaab0 <iffatmaab11@gmail.com>
2025-06-18 15:50:10 +0500

    new project

--- ArrangeFIles/ArrangingFileCode.py
+++ ArrangeFIles/ArrangingFileCode.py
@@ -3,22 +3,41 @@ import os
 def CheckPrevious(folder):
     if not os.path.exists(folder):
         os.makedirs(folder)
+
+def move(folderName, files):
+    for file in files:
+        os.replace(file, f"{folderName}/{file}")       
     
 files =os.listdir()
+files.remove("ArrangingFileCode.py")
+print(files)
 
 CheckPrevious('Images')
 CheckPrevious('Docs')
 CheckPrevious('Media')
+CheckPrevious('Others')
 
 #sending images of mentioned type in images folder
 imageExt=[".jpg", ".jpeg", ".png"]
-images=[file for file in files if os.path.splitext(file)[1].lower in imageExt]
+images=[file for file in files if os.path.splitext(file)[1].lower() in imageExt]
 
 #sending documents of mentioned type in Docs folder
 docExt=[".pdf",".pptx",".py",".cpp"]
-images=[file for file in files if os.path.splitext(file)[1].lower in docExt]
+Docs=[file for file in files if os.path.splitext(file)[1].lower() in docExt]
 
 #sending Videos of mentioned type in Media folder
 medExt=[".mp4",".mp3",".flv"]
-images=[file for file in files if os.path.splitext(file)[1].lower in medExt]
+media=[file for file in files if os.path.splitext(file)[1].lower() in medExt]
+
+others=[]
+for file in files:
+    ext=os.path.splitext(file)[1].lower()
+    if (ext not in imageExt) and (ext not in docExt) and(ext not in medExt) and os.path.isfile(file):
+        others.append(file)
+
+
+move("Media",media)
+move("Images",images)
+move("Docs",Docs)
+move("Others",others)
 
