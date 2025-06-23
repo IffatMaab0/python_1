f87fbdafa14029afad1073ba433b7c4c2e3b7b0f
IffatMaab0 <iffatmaab11@gmail.com>
2025-06-24 04:53:21 +0500

    modified

--- Projects/PasswordGen/GenCode.py
+++ Projects/PasswordGen/GenCode.py
@@ -1,11 +1,10 @@
-## Author : Iffat Maab
-## Date   : 18th June 2025
+
 ## Description : Password Generator in Python
 
-import string          ## Imports string module for predefined character sets
-import random          ## import it for shuffling
+import string          
+import random          
 
-##Predefined constants from the string module
+
 s1=string.ascii_lowercase            
 s2=string.ascii_uppercase            
 s3=string.digits
@@ -13,7 +12,7 @@ s4=string.punctuation
 
 plen=int(input("Enter the length of password: ")) 
 
-# List to store all characters
+
 s=[]       
 
 s.extend(list(s1))
@@ -24,7 +23,7 @@ s.extend(list(s4))
 # Shuffle the character list from random module
 random.shuffle(s)         
 
-# Join the first 'plen' characters into a string
+
 pswd= ("".join(s[0:plen])) 
 print("Your Passwrod is: ", pswd)
 
