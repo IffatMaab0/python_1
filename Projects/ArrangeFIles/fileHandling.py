b6587d1285900313ec7cc6da4f42dec78d23a9da
IffatMaab0 <iffatmaab11@gmail.com>
2026-04-05 17:00:48 +0500

    completed

--- Projects/ArrangeFIles/fileHandling.py
+++ Projects/ArrangeFIles/fileHandling.py
@@ -42,12 +42,53 @@ def rewrite_file(filename):
     try:
         with open (filename , "w") as f:
             content = input("Type to Add data: ")
-            f.write(content )
-            print ( f "\n Data added to the {filename}")
+            f.write(content)
+            print (f"Data added to the {filename}")
     except FileNotFoundError:
         print(f"{filename} not found")
     except Exception as e:
         print("An Error Occured!")  
 
-d          
+def main():
+    while True:
+        print("--File Management System--")
+        print("Type 0 --> To Exit The System")
+        print("Type 1 --> To Create File")
+        print("Type 2 --> To View All Files")
+        print("Type 3 --> To Delete File")
+        print("Type 4 --> To Read File")
+        print("Type 5 --> To Rewrite File")
+
+        choice = input("Type your choice(0-6): ")
+
+        if choice == "0":
+            print("Exiting..")
+            break
+
+        elif choice == "1":
+            filename = input("Type the File-Name to create: ")
+            create_file(filename)
+
+        elif choice == "2":
+            view_all_files() 
+
+        elif choice == "3":
+            filename = input("Type the File-Name to Delete: ") 
+            delete_file(filename) 
+
+        elif choice == "4":
+            filename = input("Type the File-Name to Read: ")   
+            read_file(filename)
+
+        elif choice == "5":
+            filename = input("Type the File-Name to Write: ") 
+            rewrite_file(filename)  
+        else:
+            print("In-Valid Syntax!")
+
+if __name__ =="__main__"  :
+    main()              
+
+
+
 
