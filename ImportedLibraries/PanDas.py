ff0963d4c757966c2b47eb04f443995deb2fe1cb
IffatMaab0 <iffatmaab11@gmail.com>
2026-04-15 19:58:05 +0500

    added

--- ImportedLibraries/PanDas.py
+++ ImportedLibraries/PanDas.py
@@ -17,7 +17,11 @@ import pandas as pd
 # dict_series = pd.Series(marks)
 
 # csv reading
-sub = pd.read_csv(r"C:\Users\City Computer\Desktop\VS\python\ImportedLibraries\bollywood.csv")
+mov = pd.read_csv(r"C:\Users\City Computer\Desktop\VS\python\ImportedLibraries\bollywood.csv")
+mov = mov.squeeze()
+vk = pd.read_csv(r"C:\Users\City Computer\Desktop\VS\python\ImportedLibraries\kohli_ipl (1).csv")
+vk= vk.squeeze()
+sub = pd.read_csv(r"C:\Users\City Computer\Desktop\VS\python\ImportedLibraries\subs.csv")
 sub = sub.squeeze()
 # print(sub)
 
@@ -25,4 +29,13 @@ sub = sub.squeeze()
 # print(sub.tail(3))
 
 # print(sub["lead"].value_counts())
-# eror print(sub.sort_values(by = "lead" , ascending = False)).head(1).value[0] #function chaining
+# print(sub.sort_values(by = "lead" , ascending = False)).head(1).value[0] #function chaining
+# print(vk.sort_values(ascending= False).head(1).value[0])
+
+### Series with python functionalities
+# print(len(sub))     #len, size , sorted(give output in list), min, max relational
+import matplotlib.pyplot as plt
+# plt.plot(sub)
+# plt.show()
+mov.value_counts("lead").head(20).plot(kind="bar")
+plt.show()
