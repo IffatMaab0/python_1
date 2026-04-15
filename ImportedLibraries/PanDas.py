32cbc2a4467448325931ec3bfd2a5f8c238a7c20
IffatMaab0 <iffatmaab11@gmail.com>
2026-04-15 10:24:58 +0500

    modified pandas

--- ImportedLibraries/PanDas.py
+++ ImportedLibraries/PanDas.py
@@ -15,3 +15,14 @@ import pandas as pd
 #     "PS": 74
 # }
 # dict_series = pd.Series(marks)
+
+# csv reading
+sub = pd.read_csv(r"C:\Users\City Computer\Desktop\VS\python\ImportedLibraries\bollywood.csv")
+sub = sub.squeeze()
+# print(sub)
+
+# print(sub.head(3))
+# print(sub.tail(3))
+
+# print(sub["lead"].value_counts())
+# eror print(sub.sort_values(by = "lead" , ascending = False)).head(1).value[0] #function chaining
