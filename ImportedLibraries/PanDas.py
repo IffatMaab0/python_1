fb7d30d54a8c85c96fe97dad00fdae86abe07853
IffatMaab0 <iffatmaab11@gmail.com>
2026-04-16 20:00:06 +0500

    pandas dataframe

--- ImportedLibraries/PanDas.py
+++ ImportedLibraries/PanDas.py
@@ -41,6 +41,42 @@ import pandas as pd
 # plt.show()
 
 ###DATAFRAMES
+movies= pd.read_csv(r"C:\Users\City Computer\Desktop\VS\python\ImportedLibraries\movies.csv")
+ipl = pd.read_csv(r"C:\Users\City Computer\Desktop\VS\python\ImportedLibraries\ipl-matches.csv")
+student_dict = {'name':['a','b','c','d','e','f'],
+                'iq':[100,80,120,70,0,0],
+                'marks':[90,70,100,50,0,0],
+                'package':[10,7,14,5,0,0]}
+student= pd.DataFrame(student_dict, columns=['name','iq','marks','package'])
+student.set_index('name', inplace=True)
 
+# print(movies)
+# print(movies.describe())
+# print(movies.info())
+# print(movies.duplicated().sum())
+
+##selecting cols
+# print(movies['title_x'])   #series
+# print(movies[['title_x','year_of_release','runtime' ]])   #dataframe
+# print(ipl[['Team1','Team2','WinningTeam']])
+##selecting rows
+# print(movies.iloc[0:5])
+# print(student.loc["a"])
+# print(student.loc['a':'c'])
+#indexing position ->iloc
+#index name ->loc   but still default index could be used
+
+##selecting both rows and cols
+# print(movies.iloc[0:3,0:3])
+
+###filtering DataFrame
+#1-find all final winners
+# val = ipl['MatchNumber'] =='Final'
+# print(val)
+# new_df= ipl[val]
+# print(new_df)
+# print(new_df[['Season','WinningTeam']])
+#print(ipl[ipl['MatchNumber']=='Final'])[['Season','WinningTeam']] -->single line
+#2-
 
 
